1fe629ce9a5ad92922ff9a20e8c85353eee6084e
yan zhao <yzhao322@gmail.com>
2020-03-23 17:37:20 -0700

    sql files

--- db/schema.sql
+++ db/schema.sql
@@ -2,4 +2,7 @@
 DROP DATABASE IF EXISTS user_db;
 -- Creates the "blogger" database --
 CREATE DATABASE user_db;
+drop table users;
 select * from users;
+update users set title = "Manager"
+where email = "yan@123.com";
