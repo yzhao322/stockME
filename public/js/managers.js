280f6d9c1969bf21e898e7f4e1f45d1eb26abb1c
yan zhao <yzhao322@gmail.com>
2020-03-24 20:51:43 -0700

    add master page

--- public/js/managers.js
+++ public/js/managers.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     const addNotesToAccount = $("form.create-notes");
     const notesContent = $("textarea#account-notes");
     const account = $("input#username");
+    const upgrade = $("form.upgrade");
+    const degrade = $("form.degrade");
 
     viewDetails.on("click", function (event) {
         event.preventDefault();
@@ -20,9 +22,20 @@ $(document).ready(function () {
     addNotesToAccount.on("submit", function (event) {
         event.preventDefault();
         let username = account.val();
-        let notes= notesContent.val().trim(); 
+        let notes = notesContent.val().trim();
         addNotesTo(username, notes);
-    })
+    });
+    
+    upgrade.on("submit", function (event) {
+        event.preventDefault();
+        let username = this.name;
+        upgradeToManager(username);
+    });
+    degrade.on("submit", function (event) {
+        event.preventDefault();
+        let username = this.name;
+        degradeToMember(username);
+    });
 
     function getDetail(username) {
         $.get("/api/user/" + username).then(function (data) {
@@ -49,6 +62,33 @@ $(document).ready(function () {
                 console.log(Err);
             });
     }
+    function upgradeToManager(username) {
+        $.ajax({
+            method: "PUT",
+            url: "/api/user/title/" + username,
+            data: { title: "Manager"}
+          })
+            .then(function () {
+                window.location.replace("/master");
+            })
+            .catch((Err) => {
+                console.log(Err);
+            });
+    }
+
+    function degradeToMember(username) {
+        $.ajax({
+            method: "PUT",
+            url: "/api/user/title/" + username,
+            data: { title: "Member"}
+          })
+            .then(function () {
+                window.location.replace("/master");
+            })
+            .catch((Err) => {
+                console.log(Err);
+            });
+    }
 
     function deleteThisAccount(username) {
         $.ajax({
@@ -62,8 +102,6 @@ $(document).ready(function () {
                 console.log(Err);
             });
     }
-
-
 
 
 })
