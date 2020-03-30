07344563d6399cf48fc07d84581339a4fd19256a
yan zhao <yzhao322@gmail.com>
2020-03-29 17:12:08 -0700

    edit db file

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,9 @@ DROP DATABASE IF EXISTS user_db;
 -- Creates the "blogger" database --
 CREATE DATABASE user_db;
 use user_db;
-select * from users;
-select * from stocks;
-select * from stockpurchasedbyusers;
 update users set title = "Manager"
-where email = "yan@234.com";
+where email = "yan@123.com";
 update users set title = "Master"
-where email = "saint@123.com";
-drop table stocks;
-drop table users;
-select * from StockPurchasedByUsers;
-drop table StockPurchasedByUsers;
-update StockPurchasedByUsers set purchasePrice = 2000
-where id = 1;
+where email = "yan@234.com";
+
 
