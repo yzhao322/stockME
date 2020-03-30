47d90febe9b89aae442b9a2d043d80c0ebbf816e
Eric Zeng <38875467+xzengeric@users.noreply.github.com>
2020-03-29 19:09:37 -0700

    update manager.js and css

--- public/js/managers.js
+++ public/js/managers.js
@@ -1,47 +1,48 @@
-
-
-$(document).ready(function () {
+$(document).ready(function() {
     const viewDetails = $(".view-details");
     const deleteAccount = $(".delete-account");
-    const addNotesToAccount = $("form.create-notes");
     const notesContent = $("textarea#account-notes");
     const account = $("input#usernameByInput");
     const upgrade = $("form.upgrade");
     const degrade = $("form.degrade");
 
-    var username;
-    var username2;
-   
 
-    $("span").hide();
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var addNotebtn = document.getElementById("submit");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
     //styling pages
     $("span").hide();
-    
+
     $(".user-list-info").hide();
-    $("li#user-list-info").click(function (event) {
+    $("li#user-list-info").click(function(event) {
         event.preventDefault();
         $('.user-list-info').toggle("slide");
     });
-    
+
     $(".account-management").hide();
-    $("li#account-management").click(function (event) {
+    $("li#account-management").click(function(event) {
         event.preventDefault();
         $('.account-management').toggle("slide");
     });
-    
+
     $("form.notePanel").hide();
-    $("li#notePanel").click(function (event) {
+    $("li#notePanel").click(function(event) {
         event.preventDefault();
         $('form.notePanel').toggle("slide");
     });
 
-    account.on("click", function (event) {
+    account.on("click", function(event) {
         event.preventDefault();
         $("form span").show();
         $("#addNoteMsg").text("Enter a note that you want to add to this account");
     })
 
-    viewDetails.on("click", function (event) {
+    viewDetails.on("click", function(event) {
         event.preventDefault();
         let username = this.name;
         getDetail(username);
@@ -49,57 +50,64 @@ $(document).ready(function () {
 
     });
 
-    deleteAccount.on("click", function (event) {
+    addNotebtn.onclick = function() {
+
+
+        event.preventDefault();
+
+        let notes = notesContent.val().trim();
+
+        let username = $("input#usernameByInput.form-control").val();
+        let username2 = document.getElementById("usernameByInput").textContent;
+
+
+        if (username) {
+            document.getElementById("addNoteMsg").textContent = "You have updated notes to ：" + username;
+            addNotesTo(username, notes);
+
+        } else if (username2) {
+            document.getElementById("addNoteMsg").textContent = "You have updated notes to ： " + username2;
+            addNotesTo(username2, notes);
+
+
+        } else {
+            onsole.log("Nothing is selected")
+        }
+
+        modal.style.display = "block";
+
+    }
+
+
+
+    deleteAccount.on("click", function(event) {
         event.preventDefault();
         let username = this.name;
         deleteThisAccount(username);
     });
 
-    addNotesToAccount.on("submit", function (event) {
-        event.preventDefault();
-
-        let notes = notesContent.val().trim();
-        username = account.val();
-        username2 = $("#username").text();
-        console.log("username: ", username, "username2:", username2);
-
-
-        if (username != null) {
-            addNotesTo(username, notes);
-            username= username.toString();
-            $("#addNoteMsg2").text("notes are succesully updated to ", username);
-        } else {
-            addNotesTo(username2, notes);
-            username2= username2.toString();
-            $("#addNoteMsg2").text("notes are succesully updated to ", username2);
-        };
-
-        
-    });
-
-
-    upgrade.on("submit", function (event) {
+    upgrade.on("submit", function(event) {
         event.preventDefault();
         let username = this.name;
         upgradeToManager(username);
     });
-    degrade.on("submit", function (event) {
+    degrade.on("submit", function(event) {
         event.preventDefault();
         let username = this.name;
         degradeToMember(username);
     });
 
     function getDetail(username) {
-        $.get("/api/user/" + username).then(function (data) {
-            $(".user-list").text("");
-            let createdDate = $("<p>").text(data.email + " is created at: " + data.createdAt);
-            $(".user-list").append($("<hr>"));
-            $(".user-list").append(createdDate);
-            let accountNotes = $("<p>").text(data.email + "'s notes : " + data.notes);
-            $(".user-list").append(accountNotes);
-            $("#selectedUser").text("You have selected :")
-            $("#username").text(data.email);
-        })
+        $.get("/api/user/" + username).then(function(data) {
+                $(".user-list").text("");
+                let createdDate = $("<p>").text(data.email + " is created at: " + data.createdAt);
+                $(".user-list").append($("<hr>"));
+                $(".user-list").append(createdDate);
+                let accountNotes = $("<p>").text(data.email + "'s notes : " + data.notes);
+                $(".user-list").append(accountNotes);
+                $("#selectedUser").text("You have selected :")
+                $("#usernameDisplay").text(data.email);
+            })
             .catch((Err) => {
                 console.log(Err);
             });
@@ -107,21 +115,22 @@ $(document).ready(function () {
 
     function addNotesTo(username, notes) {
         $.ajax({
-            method: "PUT",
-            url: "/api/user/notes/" + username,
-            data: { notes: notes }
-        })
+                method: "PUT",
+                url: "/api/user/notes/" + username,
+                data: { notes: notes }
+            })
             .catch((Err) => {
                 console.log(Err);
             });
     }
+
     function upgradeToManager(username) {
         $.ajax({
-            method: "PUT",
-            url: "/api/user/title/" + username,
-            data: { title: "Manager" }
-        })
-            .then(function () {
+                method: "PUT",
+                url: "/api/user/title/" + username,
+                data: { title: "Manager" }
+            })
+            .then(function() {
                 window.location.replace("/master");
             })
             .catch((Err) => {
@@ -131,11 +140,11 @@ $(document).ready(function () {
 
     function degradeToMember(username) {
         $.ajax({
-            method: "PUT",
-            url: "/api/user/title/" + username,
-            data: { title: "Member" }
-        })
-            .then(function () {
+                method: "PUT",
+                url: "/api/user/title/" + username,
+                data: { title: "Member" }
+            })
+            .then(function() {
                 window.location.replace("/master");
             })
             .catch((Err) => {
@@ -145,10 +154,10 @@ $(document).ready(function () {
 
     function deleteThisAccount(username) {
         $.ajax({
-            method: "DELETE",
-            url: "/api/user/" + username
-        })
-            .then(function () {
+                method: "DELETE",
+                url: "/api/user/" + username
+            })
+            .then(function() {
                 window.location.replace("/managers");
             })
             .catch((Err) => {
@@ -157,33 +166,24 @@ $(document).ready(function () {
     }
 
     // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("submit");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() { 
-  modal.style.display = "block";
-}
 
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-  console.log(username);
-  window.location.replace("/managers");
-}
+    // When the user clicks the button, open the modal 
 
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+
+        modal.style.display = "none";
+        event.preventDefault();
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            event.preventDefault();
+            window.location.replace("/managers");
+        }
+    }
 
 })
